6c9fb5788f2b0a46d4e1b0b34587925aa3c544fb
Samuel Hoskin <1-Samuel-Hoskin@users.noreply.63fef31bbb47>
2021-06-17 20:04:15 +0100

    Majour Updat / Redesign

--- Acronyms Buster_files/application.js
+++ Acronyms Buster_files/application.js
@@ -1,27 +1,18 @@
 /* global $ */
 
-// Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+function searchClick() {
+  let searchBox = document.getElementById('searchBox').value;
+  searchBox = searchBox.toLowerCase();
+  let aList = document.getElementsByClassName('govuk-table__row');
+
+  for (i = 1; i < aList.length; i++) { 
+    
+    if (!aList[i].innerHTML.toLowerCase().includes(searchBox)) {
+      aList[i].style.display="none";
+    }
+    else {
+      aList[i].style.display="govuk-table__row";                 
+    }
+  }
 }
 
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-})
-
-const fs = require('fs'); 
-const csv = require('csv-parser');
-
-fs.createReadStream('adata.csv')
-.pipe(csv())
-.on('data', function(data){
-    try {
-        console.log("Name is: "+data.NAME);
-        console.log("Age is: "+data.AGE);
-
-        //perform the operation
-    }
-    catch(err) {
-        //error handler
-    }
-})
