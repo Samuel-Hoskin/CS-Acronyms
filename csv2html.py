71c58bcda4f87e9507723d6e6c5a0e798a1a017b
Matt Copperwaite <matt@copperwaite.net>
2021-12-02 10:50:39 +0000

    using jinja2 to render

--- csv2html.py
+++ csv2html.py
@@ -1,30 +1,12 @@
 import pandas as pd
+import jinja2
 
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSf5yPnrU5Z_iIP7L3op6deIm5PeTrsr7RYgc82oBxzh4ZPoIa-JhTNr4YKECQA_AxDM616oVPPjQbr/pub?output=csv"
 data = pd.read_csv(url)
 data = data.where(pd.notnull(data), "")
-txt = open('data.txt', 'a')
-txt.truncate(0)
 
-for index, row in data.iterrows():
+out = open("index.html", "w")
 
-    a = row['Acronym']
-    b = row['Definition']
-    c = row['Meaning']
-    d = row['Department']
-    e = row['Team/Area']
-
-    y = len(c) - 500
-    if y > 0:
-        c = c[:-y]
-
-
-    first = '<tr class="govuk-table__row"><th scope="row" class="govuk-table__header">'
-    second = '</th>'
-    end = '<td class="govuk-table__cell">'
-    end2 = '</td>'
-    se = '],'
-
-    x = first + a + second + end + str(b) + end2 + end + end2 + end + str(d) + end2 + end + str(e) + end2
-#    x = first + a + second + end + b + end2 + end + end2 + end + d + end2 + end + e + end2
-    txt.write(str(x) + '\n')
+out.write(jinja2.Environment(
+        loader=jinja2.FileSystemLoader('.')
+    ).get_template("index.html.j2").render({'acronyms': data.items()}))
