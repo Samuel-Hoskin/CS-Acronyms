d00376afee9efea979e5cda688ae4ef912e4b18e
Samuel Hoskin <66562886+Samuel-Hoskin@users.noreply.github.com>
2022-08-11 21:01:27 +0100

    Grid.JS

---
+++ siteAssets/index.js
@@ -0,0 +1,10 @@
+new gridjs.Grid({
+    columns: ["Name", "Email", "Phone Number"],
+    data: [
+      ["John", "john@example.com", "(353) 01 222 3333"],
+      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+      ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+      ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+      ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+    ]
+  }).render(document.getElementById("wrapper"));
